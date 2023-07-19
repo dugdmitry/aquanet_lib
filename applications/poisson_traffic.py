4c356a168d806082c18cd5c9532a3e8702ed1298
Dmitrii Dugaev <dugdmitry@gmail.com>
2023-07-18 22:17:07 -0400

    added message construction

--- applications/poisson_traffic.py
+++ applications/poisson_traffic.py
@@ -17,6 +17,9 @@ The app also receives incmoing messages from AquaNet and gathers basic statistic
 
 
 from threading import Thread
+import struct
+import string
+import random
 
 
 # Import aquanet_lib module
@@ -31,16 +34,65 @@ AQUANET_BASE_FOLDER = "/home/dmitrii/aquanet_lib"
 LAMBDA = 0.01       # pkts/sec
 
 
+# create, serialize/deserialize a message with the following format:
+# | SRC_ID | SEQ_NO |    PAYLOAD     |   CRC   |
+# |  1Byte | 4Bytes |   1-N Bytes    |  1Byte  |
+class Message:
+    def __init__(self, src_id, seq_no, payload, crc=0):
+        self.src_id = src_id
+        self.seq_no = seq_no
+        self.payload = payload
+        self.crc = crc
+
+    def calculate_crc(self):
+        crc_value = (self.src_id + self.seq_no + sum(ord(char) for char in self.payload)) % 256
+        return crc_value
+
+    def toBytes(self):
+        payload_length = len(self.payload)
+        self.crc = self.calculate_crc()
+        message_format = '>BI{}sB'.format(payload_length)
+        print(message_format)
+        message_data = struct.pack(message_format, self.src_id, self.seq_no, self.payload.encode(), self.crc)
+        return bytearray(message_data)
+
+    @classmethod
+    def fromBytes(self, message_bytearray):
+        message_format = '>BI'
+        src_id, seq_no = struct.unpack(message_format, message_bytearray[:5])
+        payload_length = len(message_bytearray[5:-1])
+        payload_format = '>{}sB'.format(payload_length)
+        payload, crc = struct.unpack(payload_format, message_bytearray[5:])
+        return Message(src_id, seq_no, payload, crc)
+
 # return random delay in milliseconds, according to Poisson/exponential distribution
 def poisson_ms(l):
-    return 1
+    return 1000
 
 
-# generate next random message with pre-defined message format:
-# | SRC_ID | SEQ_NO | RANDOM_PAYLOAD |   CRC    |
-# |  1Byte | 4Bytes |   X Bytes      |  4Bytes  |
-def generateMsg():
-    return 0
+# generate next random message
+def generateMsg(src_id, seq_no, str_length):
+    def generate_random_string(length):
+        letters = string.ascii_letters + string.digits + string.punctuation
+        return ''.join(random.choice(letters) for _ in range(length))
+
+    # payload = "Hello, World!"
+    payload = generate_random_string(str_length)
+
+    # Creating a message object
+    message = Message(src_id, seq_no, payload)
+
+    # Converting the message to a bytearray
+    message_bytearray = message.toBytes()
+    print(message_bytearray)
+    print(len(message_bytearray))
+
+    # Converting the bytearray back to a message object
+    reconstructed_message = Message.fromBytes(message_bytearray)
+    print(reconstructed_message.src_id)
+    print(reconstructed_message.seq_no)
+    print(reconstructed_message.payload)
+    print(reconstructed_message.crc)
 
 
 # receive thread
@@ -72,6 +124,7 @@ def main():
     try:
         i = 0
         while True:
+            generateMsg(AQUANET_NODE_ID, i, 44)
             aquaNetManager.send(("hello: " + str(i)).encode(), AQUANET_DEST_ID)
             time.sleep(1)
             i += 1
